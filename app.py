13f01180b4232cbc8326a73756734693ab062471
Loric Brevet <loric.brevet@gmail.com>
2017-02-01 23:56:20 +0100

    Start implementing mechanism

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import config
 import db
 import pprint
 import auth
+#import gif
 
 app = Flask(__name__)
 app.debug = True
@@ -15,7 +16,7 @@ toolbar = DebugToolbarExtension(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', has_pairs=db.has_pairs(), config=db.get_config())
 
 @app.route("/edit", methods=['POST', 'GET'])
 @auth.requires_auth
@@ -36,6 +37,7 @@ def edit():
     return render_template('edit.html', items=items)
 
 @app.route('/remove/<id>')
+@auth.requires_auth
 def remove(id):
     db.remove_item(id)
     return redirect(url_for('edit'))
@@ -46,7 +48,20 @@ def run():
     if request.method == 'POST':
         db.set_message_before(request.form['message-before'])
         db.set_message_after(request.form['message-after'])
-    return render_template('run.html', config=db.get_config())
+    return render_template('run.html', config=db.get_config(), has_pairs=db.has_pairs())
+
+@app.route('/generate')
+@auth.requires_auth
+def generate():
+    db.add_pair({'test': True})
+    #gif.generate()
+    return redirect(url_for('run'))
+
+@app.route('/reset')
+@auth.requires_auth
+def reset():
+    db.reset_pairs()
+    return redirect(url_for('run'))
 
 @app.template_filter('upload_dir')
 def prepend_upload_path(filename):
